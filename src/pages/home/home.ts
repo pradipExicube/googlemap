61f0afe21b8783c7858bdb118983b55a75c38a23
Pradip Mondal <pradip@Sudiptas-MacBook-Air.local>
2018-05-30 14:42:50 +0530

    night mode on

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,20 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
 import { GoogleMap, GoogleMaps, MyLocation, Marker, GoogleMapsAnimation, GoogleMapsEvent, GoogleMapOptions } from '@ionic-native/google-maps';
+
+import {maptypeStyle} from '../../common/maptype';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public map: GoogleMap;
 
+  public map: GoogleMap;
+  public changemap:any;
+  public mapBackground:boolean;
+  
 
   constructor(public navCtrl: NavController,
      public platform:Platform,
+     public zone:NgZone
 
     ) {
+      this.mapBackground = true;
+      this.changemap = maptypeStyle
+  }
 
+  mapbackgroundchange(){
+    this.zone.run(()=>{
+      this.map.setOptions({
+        styles: this.mapBackground === true? this.changemap[0] : this.changemap[1],
+      })
+      //this.addMapMarker();
+    })
   }
 
   ngAfterViewInit() {
@@ -23,16 +40,17 @@ export class HomePage {
     });
   }
 
+
+
   loadMap() {
     let options: GoogleMapOptions = {
-    
+      mapType:'MAP_TYPE_TERRAIN',
       controls: {
         'compass': false,
-       // 'myLocationButton': true,
-        //'myLocation': true,   // (blue dot)
+        'myLocationButton': false,
+        'myLocation': true,   // (blue dot)
         'indoorPicker': true,
-        //'zoom': true,          // android only
-        'mapToolbar': true,     // android only
+        'mapToolbar':false
       },
     
       gestures: {
@@ -42,45 +60,53 @@ export class HomePage {
         rotate: true,
         mapTypeControl:true
       },
-      'styles': [
-        {
-          featureType: "all",
-          stylers: [
-            { saturation: -80 }
-          ]
-        },{
-          featureType: "road.arterial",
-          elementType: "geometry",
-          stylers: [
-            { hue: "#000" },
-            { saturation: 50 }
-          ]
-        },{
-          featureType: "poi.business",
-          elementType: "labels",
-          stylers: [
-            { visibility: "off" }
-          ]
-        }
-      ],
-      // styles: [], // https://developers.google.com/maps/documentation/javascript/style-reference
+      styles: this.changemap[0],
       preferences: {
         building: true
       },
-      camera: {
-        target: {
-          lat: 22.6881931,
-          lng: 88.4711671
-        },
-        zoom: 18,
-        tilt: 30
-      }
     };
 
     this.map = GoogleMaps.create('map', options);
+    setTimeout(() => {
+      this.addMapMarker();
+    }, 1000);
 
   }
 
+addMapMarker(){
+  this.map.clear();
+
+  // Get the location of you
+  this.map.getMyLocation()
+    .then((location: MyLocation) => {
+      // console.log(JSON.stringify(location, null ,2));
+
+      this.map.animateCamera({
+        target: location.latLng,
+        zoom: 17,
+        tilt: 30,
+       // bearing:140,
+        //duration:3000
+      })
+      .then(() => {
+
+        // add a marker
+        let marker: Marker = this.map.addMarkerSync({
+          title: '@ionic-native/google-maps plugin!',
+          snippet: 'This plugin is awesome!',
+          position: location.latLng,
+          animation: GoogleMapsAnimation.BOUNCE,
+          icon:{
+            'url':'assets/imgs/markerimg.png'
+          }
+        });
+
+        marker.addEventListener(GoogleMapsEvent.MARKER_CLICK).subscribe((markerParams) => {
+          // marker.showInfoWindow();
+        });
+     });
+    });
+}
 
 
 }
